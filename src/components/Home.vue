7f1be5738c990f0e95d13ff2340bf459df54e427
chenghz98 <2313902108@qq.com>
2021-01-29 16:35:05 +0800

    16_配置Home下的用户列表二级路由

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,29 +9,36 @@
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体 -->
-
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside :width="isCollapse ? '64px' : '200px'">
+      <el-aside :width="isCollapse ? '64px': '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened :collapse="isCollapse" :collapse-transition="false" router>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+        >
           <!-- 一级菜单 -->
-          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+          <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
               <i :class="iconsObj[item.id]"></i>
               <!-- 文本 -->
-              <span>{{ item.authName }}</span>
+              <span>{{item.authName}}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" :key="subItem.id">
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
                 <!-- 文本 -->
-                <span>{{ subItem.authName }}</span>
+                <span>{{subItem.authName}}</span>
               </template>
             </el-menu-item>
           </el-submenu>
@@ -70,15 +77,15 @@ export default {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
-    // 切换菜单的折叠与展开
-    toggleCollapse() {
-      this.isCollapse = !this.isCollapse
-    },
     // 获取所有的菜单
     async getMenuList() {
       const { data: res } = await this.$http.get('menus')
       if (res.meta.status !== 200) this.$message.error(res.meta.msg)
       this.menulist = res.data
+    },
+    // 切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -106,7 +113,9 @@ export default {
 }
 .el-aside {
   background-color: #333744;
-  border-right: none;
+  .el-menu {
+    border-right: none;
+  }
 }
 .el-main {
   background-color: #eaedf1;
